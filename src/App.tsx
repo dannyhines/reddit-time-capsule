77c17222c436f9e4435038fde10018e02977b546
Daniel Hines <daniel.c.hines@vanderbilt.edu>
2021-08-21 22:41:46 -0500

    title + logo

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          It worked!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://reddit.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Reddit
         </a>
       </header>
     </div>
