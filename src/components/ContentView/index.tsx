dee2fcad51c517d1881853c4b5b1685c606aaea9
Daniel Hines <daniel.c.hines@vanderbilt.edu>
2021-08-23 23:10:32 -0500

    add r/politics

--- src/components/ContentView/index.tsx
+++ src/components/ContentView/index.tsx
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from "react";
 import { BackTop, Row, Col, Spin, Divider } from 'antd';
 import DateSelectionView from '../DateSelector';
 import './ContentView.css'
-import NewsView from "../NewsView";
+import ListView from "../ListView";
 import { Post } from "../../types/Post";
 import dayjs, { Dayjs } from 'dayjs';
 import ImageCard from "../ImageCard";
 import useWindowDimensions from "../../utils/useWindowDimensions";
+import ListTitle from "../common/ListTitle";
 
 interface ContentViewProps {
 }
@@ -18,31 +19,35 @@ const ContentView: React.FC<ContentViewProps> = (props) => {
     const [news, setNews] = useState<Post[]>([])
     const [memes, setMemes] = useState<Post[]>([])
     const [pics, setPics] = useState<Post[]>([])
+    const [politics, setPolitics] = useState<Post[]>([])
 
     function dateChanged(value: number) {
         setStartDate(value)
+        setLoading(true)
     }
 
     useEffect(() => {
         const endDate = (startDate || new Date().getTime()) + 86400
         // console.log("making request with startDate ", startDate, ", endDate: ", endDate)
-        const url = `https://api.pushshift.io/reddit/search/submission/?q=&after=${startDate}&before=${endDate}&metadata=false&frequency=hour&advanced=false&sort=desc&sort_type=score&size=10&subreddit=`;
+        const url = `https://api.pushshift.io/reddit/search/submission/?q=&after=${startDate}&before=${endDate}&metadata=false&frequency=hour&advanced=false&sort=desc&sort_type=score&size=8&subreddit=`;
 
         const fetchData = async () => {
             setLoading(true)
             try {
-                const newsResponse = await fetch(url + 'news');
+                const newsResponse = await fetch(url + 'news,worldnews');
                 const memesResponse = await fetch(url + 'memes,memeeconomy,dankmemes');
                 const picsResponse = await fetch(url + 'pics');
+                const politicsResponse = await fetch(url + 'politics');
 
                 const newsJson = await newsResponse.json();
                 const memesJson = await memesResponse.json();
                 const picsJson = await picsResponse.json();
+                const politicsJson = await politicsResponse.json();
 
-                // console.log("success, response: ", json.data);
                 setNews(newsJson.data.slice(0, 10))
-                setMemes(memesJson.data.slice(0, 8))
+                setMemes(memesJson.data.slice(0, 10))
                 setPics(picsJson.data.slice(0, 8))
+                setPolitics(politicsJson.data.slice(0, 8))
 
             } catch (error) {
                 console.log("error fetching posts: ", error);
@@ -67,21 +72,23 @@ const ContentView: React.FC<ContentViewProps> = (props) => {
 
             <DateSelectionView handleSubmit={dateChanged} />
 
-            <div style={{ textAlign: 'center', paddingTop: 16 }}>
-                <Spin spinning={loading} size="large" />
-                {!loading &&
+            <div style={{ textAlign: 'center', paddingTop: 16, minHeight: 500 }}>
+
+                {loading ? <Spin spinning={loading} size="large" /> :
                     <div>
                         <Divider style={{ borderTopColor: '#636363' }}>
-                            {width > 600 ? <h1>{stringDate}</h1> : <h3>{stringDate}</h3>}
+                            {width > 450 ? <h1>{stringDate}</h1> : <h3>{stringDate}</h3>}
                         </Divider>
                         <Row gutter={16} justify='center'>
                             <Col lg={8} md={12} xs={24}>
-                                <NewsView stories={news} />
+                                <ListView title="News" posts={news} />
+                                <br />
+                                <ListView title="Politics" posts={politics} />
                             </Col>
                             <Col lg={8} md={12} xs={12}>
-                                <h2>
-                                    r/Pics
-                                </h2>
+                                <ListTitle>
+                                    Pictures
+                                </ListTitle>
                                 {pics.filter(x => x.url.length).map((item) => (
                                     <ImageCard
                                         key={item.id}
@@ -91,9 +98,9 @@ const ContentView: React.FC<ContentViewProps> = (props) => {
 
                             </Col>
                             <Col lg={8} md={12} xs={12}>
-                                <h2>
-                                    r/Memes
-                                </h2>
+                                <ListTitle>
+                                    Memes
+                                </ListTitle>
                                 {memes.filter(x => x.url.length).map((item) => (
                                     <ImageCard
                                         key={item.id}
---
+++ src/components/ListView/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Avatar, Card, List } from 'antd'
+import { Post } from "../../types/Post";
+import ListTitle from "../common/ListTitle";
+
+interface NewsViewProps {
+    title: string
+    posts?: Post[]
+}
+
+const ListView: React.FC<NewsViewProps> = (props) => {
+    const { title, posts } = props;
+    return (
+        <>
+            <ListTitle>
+                {title}
+            </ListTitle>
+
+            <Card style={{ textAlign: 'left' }}>
+                <List
+                    itemLayout="horizontal"
+                    dataSource={posts}
+                    renderItem={item => (
+                        <a href={item.full_link}>
+                            <List.Item>
+                                <List.Item.Meta
+                                    avatar={<Avatar shape="square" src={item.thumbnail} size='large' />}
+                                    title={<a href={item.url}>{item.title}</a>}
+                                    description={`r/${item.subreddit} · ${item.author} · ${item.score?.toString()} pts`}
+                                />
+                            </List.Item>
+                        </a>
+                    )}
+                />
+            </Card>
+        </>
+    );
+};
+
+export default ListView;
