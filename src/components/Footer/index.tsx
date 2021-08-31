a110686101d946357711d7d2e57560b621bc36ba
Daniel Hines <daniel.c.hines@vanderbilt.edu>
2021-08-31 10:18:35 -0500

    footer includes link to pushshift

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,4 +1,4 @@
-import { Row } from "antd";
+import { Col, Row } from "antd";
 import React from "react";
 import './Footer.css'
 
@@ -8,10 +8,17 @@ interface HeaderProps {
 const Footer: React.FC<HeaderProps> = (props) => {
     return (
         <div className="rtc-footer">
-            <Row justify='center' align='middle'>
-                <p className="footer-text">
-                    Reddit Time Capsule © 2021 · Created by Danny Hines
-                </p>
+            <Row justify='center' align='middle' style={{ padding: '1rem 0' }}>
+                <Col span={24}>
+                    <p className="footer-text">
+                        Reddit Time Capsule © 2021 · Created by Danny Hines
+                    </p>
+                </Col>
+                <Col span={24}>
+                    <p className="footer-subtitle">
+                        Data comes from <a href="https://pushshift.io" target="_blank" rel="noreferrer">pushshift.io</a>
+                    </p>
+                </Col>
             </Row>
         </div>
     );
